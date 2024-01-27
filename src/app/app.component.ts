9fa0480b525bd12b0d80b767b3103a6e0429b6e7
Daniele-Muheim <31336903+Daniele-Muheim@users.noreply.github.com>
2024-01-27 10:05:36 +0100

    add countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,72 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NicoWorkCountdown';
+  date: any;
+  now: any;
+  targetDate: any = new Date(2024, 4, 1);
+  targetTime: any = this.targetDate.getTime();
+  difference!: number;
+  months: Array<string> = [
+    'January',
+    'February',
+    'March', 'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  currentTime: any = `${
+    this.months[this.targetDate.getMonth()]
+  } ${this.targetDate.getDate()}, ${this.targetDate.getFullYear()}`;
+
+  @ViewChild('days', { static: true }) days!: ElementRef;
+  @ViewChild('hours', { static: true }) hours!: ElementRef;
+  @ViewChild('minutes', { static: true }) minutes!: ElementRef;
+  @ViewChild('seconds', { static: true }) seconds!: ElementRef;
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.tickTock();
+      this.difference = this.calculateWorkingDaysDifference();
+
+      !isNaN(this.days.nativeElement.innerText)
+        ? (this.days.nativeElement.innerText = Math.floor(this.difference))
+        : (this.days.nativeElement.innerHTML = '<img src="https://i.gifer.com/VAyR.gif" />');
+    }, 1000);
+  }
+
+  private calculateWorkingDaysDifference(): number {
+    this.date = new Date();
+    this.now = this.date.getTime();
+
+    const targetDate = new Date(this.targetTime);
+
+    const daysDiff = Math.floor((targetDate.getTime() - this.date.getTime()) / (1000 * 60 * 60 * 24));
+    let workingDaysDiff = daysDiff - 1;
+
+    for (let i = 0; i < daysDiff; i++) {
+      const currentDate = new Date(this.date.getTime() + (i * 1000 * 60 * 60 * 24));
+      const isWeekday = currentDate.getDay() >= 1 && currentDate.getDay() <= 5;
+
+      if (!isWeekday) {
+        workingDaysDiff--;
+      }
+    }
+
+    return workingDaysDiff;
+  }
+
+  tickTock() {
+    this.date = new Date();
+    this.now = this.date.getTime();
+
+    this.days.nativeElement.innerText = Math.floor(this.difference);
+    this.hours.nativeElement.innerText = 23 - this.date.getHours();
+    this.minutes.nativeElement.innerText = 60 - this.date.getMinutes();
+    this.seconds.nativeElement.innerText = 60 - this.date.getSeconds();
+  }
 }
